4ab6154f3d63fd597ccd78c2ddcf6a09facb7264
Antoine Beaulieu Savard <antoine30@outlook.com>
2017-12-08 13:18:11 -0500

    php amélioré 2

--- web/sw.js
+++ web/sw.js
@@ -25,7 +25,8 @@ self.addEventListener('install', function(event) {
                 './assets/images/strawberry.png',
 
                 './assets/js/main.js',
-                './assets/js/Quiz.js'
+                './assets/js/Quiz.js',
+                './assets/js/jquery-3.2.1.min.js'
             ]);
 
         })
