6380a4e3d1eda09ea02170b169d081b2cc5593e4
Antoine Beaulieu Savard <antoine30@outlook.com>
2017-12-08 15:28:48 -0500

    commit final vendredi service worker fonctionnne

--- web/sw.js
+++ web/sw.js
@@ -39,8 +39,6 @@ self.addEventListener('install', function(event) {
 
                 './assets/js/main.js',
                 './assets/js/Quiz.js',
-
-                './assets/js/jquery-3.2.1.min.js'
             ]);
         })
     );
